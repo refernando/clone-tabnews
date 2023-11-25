55f9746761b12f3e10f734498354ed5e5f85a5e8
refernando <75812431+refernando@users.noreply.github.com>
2023-11-25 21:34:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe, eu amo você. Se você me ama, dá uma risadinha!</h1>
+  return <h1>Mãe, eu amo você. Se você me ama, dá uma risadinha!</h1>;
 }
 
 export default Home;
